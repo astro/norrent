af4270487fe05e6c5239627b366ba49acfbbfc3b
Astro <astro@spaceboyz.net>
2010-08-28 10:36:19 +0200

    piecemap: s/pieces/buffer/

--- piecemap.js
+++ piecemap.js
@@ -2,13 +2,13 @@ var sys = require('sys');
 
 function PieceMap(length) {
     this.length = length;
-    this.pieces = new Buffer(Math.ceil(length / 8));
-    for(var i = 0; i < this.pieces.length; i++)
-	this.pieces[i] = 0;
+    this.buffer = new Buffer(Math.ceil(length / 8));
+    for(var i = 0; i < this.buffer.length; i++)
+	this.buffer[i] = 0;
 }
 
 PieceMap.prototype.get = function(idx) {
-    return (this.pieces[Math.floor(idx / 8)] << (idx % 8)) & 1 === 1;
+    return (this.buffer[Math.floor(idx / 8)] << (idx % 8)) & 1 === 1;
 };
 
 PieceMap.prototype.set = function(idx) {
